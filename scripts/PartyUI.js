2b778303702d483ccbdcdc41c2d423d9633802bc
DragnaCarta <dragnacarta@gmail.com>
2023-08-27 21:37:59 -0400

    Standardized width of party size buttons and split them into two rows of 5 for all screen widths

--- scripts/PartyUI.js
+++ scripts/PartyUI.js
@@ -17,33 +17,72 @@ class PartySizeUI {
 	initializePartySizeButtons() {
 		const fragment = document.createDocumentFragment();
 
-		// Create a flex container
-		const flexContainer = document.createElement('div');
-		flexContainer.className = 'd-flex flex-wrap justify-content-center';
+		// Create two rows
+		let buttonRowOne = document.createElement('div');
+		let buttonRowTwo = document.createElement('div');
+		buttonRowOne.className = 'row d-flex align-items-center justify-content-center';
+		buttonRowTwo.className = 'row d-flex align-items-center justify-content-center';
 
 		for (let i = 1; i <= 10; i++) {
-			const button = document.createElement('button');
-			button.className = 'btn btn-secondary m-1';
-			// button.style.margin = '1px';
-			button.textContent = i;
+			
+			const button = this.createButton(i);
+			const col = this.createColumn(button, 2);
 
-			if (i === 5) { button.classList.add('btn-success'); }
-
-			button.addEventListener('click', () => {
-				this.deselectAllButtons();
-				button.classList.add('btn-success');
-				this.encounterManager.setPartySize(i);
-			});
-
-			this.buttons.push(button);
-			flexContainer.appendChild(button);
+			if (i === 1) {
+				const padding = this.createColumn(document.createElement('div'), 1);
+				buttonRowOne.appendChild(padding);
+				buttonRowOne.appendChild(col);
+			} else if (i > 1 && i < 5) {
+				buttonRowOne.appendChild(col);
+			} else if (i === 5) {
+				const padding = this.createColumn(document.createElement('div'), 1);
+				buttonRowOne.appendChild(col);
+				buttonRowOne.appendChild(padding);
+			} else if (i === 6) {
+				const padding = this.createColumn(document.createElement('div'), 1);
+				buttonRowTwo.appendChild(padding);
+				buttonRowTwo.appendChild(col);
+			} else if (i > 6 && i < 10) {
+				buttonRowTwo.appendChild(col);
+			} else if (i === 10) {
+				const padding = this.createColumn(document.createElement('div'), 1);
+				buttonRowTwo.appendChild(col);
+				buttonRowTwo.appendChild(padding);
+			}
 		}
 
-		// Append the new row div to the fragment
-		fragment.appendChild(flexContainer);
+		// Append the two buttons rows to the fragment
+		fragment.appendChild(buttonRowOne);
+		fragment.appendChild(buttonRowTwo);
 
 		this.buttonContainer.className = 'mb-3';
 		this.buttonContainer.appendChild(fragment);
+	}
+
+	createButton(i) {
+		const button = document.createElement('button');
+		button.className = 'btn btn-secondary m-1';
+		button.style.width = '100%';
+		button.textContent = i;
+
+		if (i === 5) { button.classList.add('btn-success'); }
+
+		button.addEventListener('click', () => {
+			this.deselectAllButtons();
+			button.classList.add('btn-success');
+			this.encounterManager.setPartySize(i);
+		});
+
+		this.buttons.push(button);
+
+		return button;
+	}
+
+	createColumn(button, width) {
+		const col = document.createElement('div');
+		col.className = `col-${width} px-1`;
+		col.appendChild(button);
+		return col
 	}
 
 	deselectAllButtons() {
